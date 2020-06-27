6503a966d03ce7e384d780e8f7de6ae5c0c9138c
Felix C. Stegerman <flx@obfusk.net>
2020-06-27 20:27:23 +0200

    replace custom parse_args() w/ structopt; refactor

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 //
 //  File        : src/main.rs
 //  Maintainer  : Felix C. Stegerman <flx@obfusk.net>
-//  Date        : 2020-06-26
+//  Date        : 2020-06-27
 //
 //  Copyright   : Copyright (C) 2020  Felix C. Stegerman
 //  Version     : v0.1.2
@@ -12,53 +12,31 @@
 
 const VERSION: &str = "0.1.2";
 
-const HELP: &str = "\
-Usage: proudcat [OPTIONS] [FILES]...
-
-  proudcat-rust - cat + rainbow
-
-  Flags: {}.
-
-  Aliases: lgbt, aro, ace, bi, enby, nb, pan, trans.
-
-Options:
-  -f, --flag TEXT                 Choose which flags to use; default: pride.
-  -b, --background                Set background colour (instead of
-                                  foreground).
-  -t, --truecolor / -T, --no-truecolor
-                                  Explicitly enable/disable 24-bit colours;
-                                  default: autodetect.
-  --light                         Light terminal.
-  --demo                          Demonstrate flags.
-  --version                       Show the version and exit.
-  --help                          Show this message and exit.";
-
-const FLAGS: &str = "\
-  pride agender aromantic asexual bisexual genderfluid
-  genderqueer lesbian nonbinary pansexual polysexual transgender";
-
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::iter;
 use std::process;
 
+use structopt::StructOpt;
+
 macro_rules! oops {
   ($msg:expr)   => {{ oops($msg); panic!() }};
   ($($arg:tt)*) => {{ oops(format!($($arg)*)); panic!() }}
 }
 
-fn oops(msg: String) {
-  eprintln!("Error: {}", msg);
-  process::exit(1)
-}
+fn oops(msg: String) { eprintln!("{}", msg); process::exit(1) }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Rgb(u8, u8, u8);
 
 fn rgbto8(c: &Rgb) -> u8 {
   (16 + (c.0 as u16)*6/256*36 + (c.1 as u16)*6/256*6 + (c.2 as u16)*6/256) as u8
 }
+
+const FLAGS: &str = "\
+  pride, agender, aromantic, asexual, bisexual, genderfluid,
+  genderqueer, lesbian, nonbinary, pansexual, polysexual, transgender";
 
 // https://en.wikipedia.org/wiki/Pride_flag#Gallery
 fn flag2colours(flag: &str) -> Vec<Rgb> {                     //  {{{1
@@ -100,7 +78,7 @@ fn flag2colours(flag: &str) -> Vec<Rgb> {                     //  {{{1
 const BLACK: Rgb = Rgb(0, 0, 0);
 const WHITE: Rgb = Rgb(255, 255, 255);
 
-fn colours(flags: &Vec<String>) -> Vec<Rgb> {
+fn colours(flags: Vec<String>) -> Vec<Rgb> {
   flags.iter().flat_map(|f| flag2colours(f)).collect()
 }
 
@@ -117,8 +95,8 @@ fn with_colour(bg: bool, tc: bool, c: &Rgb, s: &str, clear: bool) -> String {
   format!("\x1b[{};{}m{}{}\x1b[{};m", sc, code, cl, s, rc)
 }
 
-fn colour(bg: bool, tc: bool, li: bool, c: &Rgb, s: &str, clear: bool)
-          -> String {
+fn colour(bg: bool, tc: bool, li: bool, c: &Rgb,
+          s: &str, clear: bool) -> String {
   let da = if li { bg } else { !bg };
   let t = if (da && brightness(c) < 0.20) || (!da && brightness(c) > 0.75) {
     with_colour(!bg, tc, if da { &WHITE } else { &BLACK }, s, clear)
@@ -131,62 +109,50 @@ fn brightness(c: &Rgb) -> f32 {
     as f32 / 255f32
 }
 
-struct Options {
-  flags: Vec<String>, bg: bool, tc: bool, light: bool, demo: bool
-}
+const HELP_TEMPLATE: &str = "\
+Usage: {usage}
 
-fn parse_args() -> (Options, Vec<String>) {                   //  {{{1
-  let mut flags = Vec::new();
-  let mut bg    = false;
-  let mut tc    = detect_truecolor();
-  let mut light = false;
-  let mut demo  = false;
-  let mut files = Vec::new();
-  let mut done  = false;
-  let mut flag  = None;
-  for arg in env::args().skip(1).collect::<Vec<String>>() {
-    if flag.is_some() {
-      for f in arg.split(",") {
-        flags.push(f.to_string())
-      }
-      flag = None
-    } else if done {
-      files.push(arg)
-    } else {
-      match arg.as_str() {
-        "--"                    => done   = true,
-        "-f" | "--flag"         => flag   = Some(arg),
-        "-b" | "--background"   => bg     = true,
-        "-t" | "--truecolor"    => tc     = true,
-        "-T" | "--no-truecolor" => tc     = false,
-        "--light"               => light  = true,
-        "--demo"                => demo   = true,
-        "--version" => {
-          println!("proudcat-rust, version {}", VERSION);
-          process::exit(0)
-        }
-        "--help" => {
-          let fs = FLAGS.lines().map(|l|
-            l.split_whitespace().collect::<Vec<_>>().join(", ")
-          ).collect::<Vec<_>>().join(",\n  ");
-          println!("{}", HELP.replace("{}", &fs));
-          process::exit(0)
-        }
-        _ => if arg.starts_with("-") {
-          oops!("no such option: {}", arg)
-        } else {
-          files.push(arg)
-        }
-      }
-    }
-  }
-  if let Some(f) = flag {
-    oops!("{} option requires an argument", f)
-  }
-  if flags.is_empty() { flags.push("pride".to_string()) }
-  if files.is_empty() { files.push("-".to_string()) }
-  (Options { flags, bg, tc, light, demo }, files)
+  proudcat-rust - cat + rainbow
+
+  Flags: {after-help}.
+
+  Aliases: lgbt, aro, ace, bi, enby, nb, pan, trans.
+
+Options:
+{unified}";
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "proudcat-rust", version = VERSION,
+  after_help = FLAGS, template = HELP_TEMPLATE)]
+struct Cli {                                                  //  {{{1
+  #[structopt(name = "flag", short = "f", long = "flag",
+    number_of_values = 1, display_order = 1,
+    help = "Choose which flags to use; default: pride")]
+  flags: Vec<String>,
+  #[structopt(short, long = "background", display_order = 2,
+    help = "Set background colour (instead of foreground)")]
+  bg: bool,
+  #[structopt(short = "t", long = "truecolor", display_order = 3,
+    help = "Explicitly enable 24-bit colours; default: autodetect")]
+  tc: bool,
+  #[structopt(short = "T", long = "no-truecolor", display_order = 4,
+    conflicts_with = "tc", help = "Explicitly disable 24-bit colours")]
+  no_tc: bool,
+  #[structopt(long, display_order = 5, help = "Light terminal")]
+  light: bool,
+  #[structopt(long, display_order = 6, help = "Demonstrate flags")]
+  demo: bool,
+  #[structopt(name = "FILES", default_value = "-")]
+  files: Vec<String>
 }                                                             //  }}}1
+
+fn parse_args() -> Cli {
+  let mut cli = Cli::from_args();
+  cli.tc      = if cli.no_tc { false } else if cli.tc { true }
+                else { detect_truecolor() };
+  if cli.flags.is_empty() { cli.flags = vec!["pride".to_string()] }
+  cli
+}
 
 // TODO
 fn stdout_isatty() -> bool {
@@ -196,36 +162,38 @@ fn stdout_isatty() -> bool {
 }
 
 fn main() {
-  let (tty, stdin)  = (stdout_isatty(), io::stdin());
-  let (opts, files) = parse_args();
-  let clrs          = colours(&opts.flags);
-  let mut it        = clrs.iter().cycle();
-  if opts.demo {
-    for flag in FLAGS.split_whitespace() {
+  let (tty, stdin, cli) = (stdout_isatty(), io::stdin(), parse_args());
+  if cli.demo {
+    for flag in FLAGS.split_whitespace().map(|x| x.trim_end_matches(",")) {
       println!("┌{}┐", "─".repeat(flag.len()));
       for c in flag2colours(flag) {
-        let s = colour(opts.bg, opts.tc, opts.light, &c, flag, false);
+        let s = colour(cli.bg, cli.tc, cli.light, &c, flag, false);
         println!("│{}│", s)
       }
       println!("└{}┘", "─".repeat(flag.len()))
     }
   } else {
-    for file in files {
+    let clrs    = colours(cli.flags.iter().flat_map(|a|
+                    a.split(",").map(|a| a.to_string())
+                  ).collect());
+    let mut it  = clrs.iter().cycle();
+    for file in cli.files {
       let mut line = String::new();
       let mut bufr = if &file == "-" {
         Box::new(stdin.lock()) as Box<dyn BufRead>
       } else {
         Box::new(io::BufReader::new(
-          File::open(file).unwrap_or_else(|e| oops!(e.to_string()))
-        ))
+          File::open(&file).unwrap_or_else(|e|
+            oops!("Could not open file: {}: {}", &file, e.to_string())
+        )))
       };
       while bufr.read_line(&mut line).unwrap() != 0 {
         let sline = line.trim();
-        if sline.is_empty() && !(opts.bg && tty) {
+        if sline.is_empty() && !(cli.bg && tty) {
           print!("{}", line)
         } else {
-          let s = colour(opts.bg, opts.tc, opts.light,
-                         it.next().unwrap(), sline, true);
+          let s = colour(cli.bg, cli.tc, cli.light, it.next().unwrap(),
+                         sline, true);
           let i = sline.chars().next().and_then(|c| line.find(c))
                                       .unwrap_or(0);
           print!("{}{}{}", line[..i].to_string(), s,
